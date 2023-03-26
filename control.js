30bfac8aa2d772c21dec1c3515de5c5b6529a7c7
Eli Weber <eliweber2001@gmail.com>
2023-03-25 22:26:13 -0400

    About page changes

--- control.js
+++ control.js
@@ -41,16 +41,3 @@ setInterval(()=>
   }
 }, 200)
 
-let latitude = 41.037660;
-    let longitude = -83.649540;
-    let mymap = L.map('mapid', {tap: false}).setView([latitude, longitude], 13);
-    const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">Open Street Map</a> contributors';
-    const tileURL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-    const tiles = L.tileLayer(tileURL, {attribution: attribution});
-    tiles.addTo(mymap);
-    var marker = L.marker([latitude, longitude]).addTo(mymap);
-    marker.bindPopup("113 East Crawford Street, Findlay, OH 45840");
-    marker.on("click", function (event) {
-        event.target.openPopup();
-     });
-    mymap.invalidateSize()
